cebb9ca3eff5f87439fd5a4ad29c5f0c641dd8c3
Hehectpboss <96016861+Hehectpboss@users.noreply.github.com>
2022-01-22 09:24:14 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -48,11 +48,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Sf_Captain">Sf_Captain</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Main Channel 👑</b> : <i><a href="https://t.me/CTP_Official/Main Channel 👑">Click Me</a></i>
 """
